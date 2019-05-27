1c90b6ace6608741b74903518a05eff325c9ad1a
Jerome Lombardi <jerome.lombardi@cases.lu>
2019-05-27 14:12:57 +0200

    change a little bit the layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { TipsTricksPage } from '../pages/tipstricks/tipstricks';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +22,7 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Tips and Tricks', component: TipsTricksPage }
     ];
 
   }
--- src/pages/tipstricks/tipstricks.ts
+++ src/pages/tipstricks/tipstricks.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
-  selector: 'page-list',
-  templateUrl: 'list.html'
+  selector: 'page-tipstricks',
+  templateUrl: 'tipstricks.html'
 })
-export class ListPage {
+export class TipsTricksPage {
   selectedItem: any;
-  icons: string[];
+  categories: string[];
+  notes: string[];
   items: Array<{title: string, note: string, icon: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
@@ -15,22 +16,22 @@ export class ListPage {
     this.selectedItem = navParams.get('item');
 
     // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+    this.categories = ['Password', 'E-mail', 'Clean workspace', 'Web', 'Waste Management'];
+    this.notes = ['Manage your password', 'Securely use your emails','','','Destroy properly your informations'];
 
     this.items = [];
-    for (let i = 1; i < 11; i++) {
+    for (let i = 0; i < this.categories.length; i++) {
       this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+        title: this.categories[i],
+        note: this.notes[i],
+        icon: 'arrow-dropright-circle'
       });
     }
   }
 
   itemTapped(event, item) {
     // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
+    this.navCtrl.push(TipsTricksPage, {
       item: item
     });
   }
