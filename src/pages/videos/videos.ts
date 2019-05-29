88c74f72f25831dbebbe7479291ff9b14612b51a
Jerome Lombardi <jerome.lombardi@cases.lu>
2019-05-29 13:45:26 +0200

    add video loading by json

--- src/pages/videos/videos.ts
+++ src/pages/videos/videos.ts
@@ -1,35 +1,53 @@
 import { Component } from '@angular/core';
-import { Loading, LoadingController, NavController, NavParams } from 'ionic-angular';
-import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
+import { Loading, LoadingController, NavController } from 'ionic-angular';
+import { DomSanitizer } from '@angular/platform-browser';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-videos',
   templateUrl: 'videos.html'
 })
 export class VideosPage {
-  video: any = {
-        url: 'https://www.youtube.com/embed/cRK-u-kuL30',
-        title: 'Virus'
-    };
+  videos: any
+  loading: Loading;
 
-    trustedVideoUrl: SafeResourceUrl;
-    loading: Loading;
-
-    constructor(public navCtrl: NavController,
-                public loadingCtrl: LoadingController,
-                private domSanitizer: DomSanitizer) {}
-
-    ionViewWillEnter(): void {
-        this.trustedVideoUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(this.video.url);
-
-        this.loading = this.loadingCtrl.create({
-            content: 'Please wait...'
-        });
-
-        this.loading.present();
+    constructor(public navCtrl: NavController,public loadingCtrl: LoadingController, private domSanitizer: DomSanitizer,) {
+      this.loadVideos().then(data => {
+        this.startIFrameLoadEvent();
+        this.videos = data;
+        this.cleanJson(this.videos);
+        this.handleIFrameLoadEvent();
+      });
     }
-
+/*
+* Load the JSON
+*/
+    loadVideos(){
+      let myrequests = new Request('../../assets/data/videos.json');
+      return fetch(myrequests).then(
+        data => {return data.json()}
+      )
+    }
+/*
+* Clean the URL
+*/
+    cleanJson(myData): void {
+        for (let item of myData) {
+            item.url = this.domSanitizer.bypassSecurityTrustResourceUrl(item.url);
+        }
+    }
+/*
+* Manage loading
+*/
     handleIFrameLoadEvent(): void {
         this.loading.dismiss();
+    }
+
+    startIFrameLoadEvent(): void {
+      this.loading = this.loadingCtrl.create({
+          content: 'Please wait...'
+      });
+
+      this.loading.present();
     }
 }
