a767e4916be6fe4bac46da85b27f2d3be5148fa9
Juan Rocha <juan.rocha@securitymadein.lu>
2019-06-04 11:52:29 +0200

    Try language management

--- src/pages/tipstricks/password/password.ts
+++ src/pages/tipstricks/password/password.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { DomSanitizer } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
 import { loadJson } from '../../../lib/utils';
 
 @Component({
@@ -10,7 +11,13 @@ import { loadJson } from '../../../lib/utils';
 export class PasswordPage {
   article:any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private domSanitizer: DomSanitizer,) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private domSanitizer: DomSanitizer,
+    private translate: TranslateService) {
+
+    console.log(this.translate.currentLang);
     loadJson('../../assets/data/tipstricks/password.json',domSanitizer).then(data => {
       this.article = data;
       console.log(this.article)
