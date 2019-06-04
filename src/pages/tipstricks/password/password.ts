9a458e00976f35296b4920cb66061cc07f03e194
Jerome Lombardi <jerome.lombardi@cases.lu>
2019-06-04 10:45:21 +0200

    first draft for json->tree with adapted style and correct various warning

--- src/pages/tipstricks/password/password.ts
+++ src/pages/tipstricks/password/password.ts
@@ -1,32 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { DomSanitizer } from '@angular/platform-browser';
+import { loadJson } from '../../../lib/utils';
 
 @Component({
   selector: 'page-password',
   templateUrl: 'password.html'
 })
 export class PasswordPage {
-  selectedItem: any;
-  categories: string[];
-  notes: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  article:any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
+  constructor(public navCtrl: NavController, public navParams: NavParams, private domSanitizer: DomSanitizer,) {
+    loadJson('../../assets/data/tipstricks/password.json',domSanitizer).then(data => {
+      this.article = data;
+      console.log(this.article)
+    });
 
-    // Let's populate this page with some filler content for funzies
-    this.categories = ['Password', 'E-mail', 'Clean workspace', 'Web', 'Waste Management'];
-    this.notes = ['Manage your password', 'Securely use your emails','','','Destroy properly your informations'];
-
-    this.items = [];
-    for (let i = 0; i < this.categories.length; i++) {
-      this.items.push({
-        title: this.categories[i],
-        note: this.notes[i],
-        icon: 'arrow-dropright-circle'
-      });
-    }
   }
 
 }
