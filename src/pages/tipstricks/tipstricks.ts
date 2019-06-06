423613602a56452197c255199b2eb0599f166f05
Juan Rocha <juan.rocha@securitymadein.lu>
2019-06-06 16:37:05 +0200

    Add icons related

--- src/pages/tipstricks/tipstricks.ts
+++ src/pages/tipstricks/tipstricks.ts
@@ -34,11 +34,11 @@ export class TipsTricksPage {
     }
     this.translate.stream(['Password', 'E-mail', 'Clean Workspace','Web', 'Waste Management']).subscribe(translations => {
       this.pages = [
-        { title: translations['Password'], url: '../assets/data/tipstricks/password.json' , article: '', links:'', icon: 'arrow-dropright-circle'},
-        { title: translations['E-mail'], url: '../assets/data/tipstricks/email.json' , article: '', links:'', icon: 'arrow-dropright-circle'},
-        { title: translations['Clean Workspace'], url: '../assets/data/tipstricks/cleanworkspace.json' , article: '', links:'', icon: 'arrow-dropright-circle'},
-        { title: translations['Web'], url: '../assets/data/tipstricks/web.json' , article: '', links:'', icon: 'arrow-dropright-circle'},
-        { title: translations['Waste Management'], url: '../assets/data/tipstricks/wastemanagement.json' , article: '', links:'', icon: 'arrow-dropright-circle'}
+        { title: translations['Password'], url: '../assets/data/tipstricks/password.json' , article: '', links:'', icon: 'key'},
+        { title: translations['E-mail'], url: '../assets/data/tipstricks/email.json' , article: '', links:'', icon: 'mail'},
+        { title: translations['Clean Workspace'], url: '../assets/data/tipstricks/cleanworkspace.json' , article: '', links:'', icon: 'desktop'},
+        { title: translations['Web'], url: '../assets/data/tipstricks/web.json' , article: '', links:'', icon: 'globe'},
+        { title: translations['Waste Management'], url: '../assets/data/tipstricks/wastemanagement.json' , article: '', links:'', icon: 'trash'}
       ];
     })
   }
