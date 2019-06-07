9363a474f5664a310e837629aeaaa4338894f21e
Juan Rocha <juan.rocha@securitymadein.lu>
2019-06-07 16:03:28 +0200

    Add custom icons

--- src/pages/tipstricks/tipstricks.ts
+++ src/pages/tipstricks/tipstricks.ts
@@ -34,10 +34,10 @@ export class TipsTricksPage {
     }
     this.translate.stream(['Password', 'E-mail', 'Clean Workspace','Web', 'Waste Management']).subscribe(translations => {
       this.pages = [
-        { title: translations['Password'], url: '../assets/data/tipstricks/password.json' , article: '', links:'', icon: 'key'},
-        { title: translations['E-mail'], url: '../assets/data/tipstricks/email.json' , article: '', links:'', icon: 'mail'},
-        { title: translations['Physical Security'], url: '../assets/data/tipstricks/physicalsecurity.json' , article: '', links:'', icon: 'desktop'},
-        { title: translations['Web'], url: '../assets/data/tipstricks/web.json' , article: '', links:'', icon: 'globe'},
+        { title: translations['Password'], url: '../assets/data/tipstricks/password.json' , article: '', links:'', icon: 'url(../assets/imgs/t&t/1.png)'},
+        { title: translations['E-mail'], url: '../assets/data/tipstricks/email.json' , article: '', links:'', icon: 'url(../assets/imgs/t&t/2.png)'},
+        { title: translations['Physical Security'], url: '../assets/data/tipstricks/physicalsecurity.json' , article: '', links:'', icon: 'url(../assets/imgs/t&t/3.png)'},
+        { title: translations['Web'], url: '../assets/data/tipstricks/web.json' , article: '', links:'', icon: 'url(../assets/imgs/t&t/4.png)'},
         //{ title: translations['Waste Management'], url: '../assets/data/tipstricks/wastemanagement.json' , article: '', links:'', icon: 'trash'}
       ];
     })
