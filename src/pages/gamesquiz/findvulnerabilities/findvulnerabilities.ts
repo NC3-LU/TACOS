a7283b3dde081c7d9fcf18f54e39b232bf82e420
Jerome Lombardi <jerome.lombardi@cases.lu>
2019-06-25 15:27:26 +0200

    little improvement

--- src/pages/gamesquiz/findvulnerabilities/findvulnerabilities.ts
+++ src/pages/gamesquiz/findvulnerabilities/findvulnerabilities.ts
@@ -1,14 +1,9 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { DomSanitizer } from '@angular/platform-browser';
 
 import { TranslateService } from '@ngx-translate/core';
 import { Storage } from '@ionic/storage';
 import { AlertController } from 'ionic-angular';
-
-import { loadJson } from '../../../lib/utils';
-import { loadRightLanguage } from '../../../lib/utils';
-
 
 @Component({
   selector: 'page-findvulnerabilities',
@@ -17,22 +12,37 @@ import { loadRightLanguage } from '../../../lib/utils';
 
 export class findVulnerabilitiesPage {
   dataGame : any = null;
+  answers : any; //answers and if they are found 1=found
+  gameScore=0; //score of the current game
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private translate: TranslateService,
-    private domSanitizer: DomSanitizer,
     private storage: Storage,
     private alertController:AlertController
      ) {
+       this.dataGame = null;
        this.dataGame = navParams.get('data');
+  }
+
+  ionViewWillEnter (){
+    this.answers = [];
+    for (let i = 0; i < this.dataGame[0].vulnerabilities.length; i++) {
+        this.answers[this.dataGame[0].vulnerabilities[i]['title']] = 0;
+    }
+    console.log(this.answers)
   }
 
   /*
   * Display a vulberability and its explanation
   */
-  vulnFound(title,explanation){
+  vulnFound(title : string,explanation : string){
+    if(this.answers[title] == 0){
+      this.gameScore++;
+      this.answers[title] = 1;
+      this.storage.set(this.dataGame[0].storageKey,this.gameScore/(this.dataGame[0].vulnerabilities.length)); // save the global score of the quiz
+    }
     let alert = this.alertController.create({
       title: title,
       subTitle:explanation,
