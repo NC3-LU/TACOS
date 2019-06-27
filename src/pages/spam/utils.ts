3a550b9756a97d384a868d3fd7478f6490150b0e
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-06-27 08:00:11 +0200

    removed commented code

--- src/pages/spam/utils.ts
+++ src/pages/spam/utils.ts
@@ -32,34 +32,4 @@ export class UtilsService {
         });
     }
 
-    /*
-    * Retrieve the list of recently added spam from the server.
-    * @return : array of spam
-    */
-    // loadSpamsLight() {
-    //     let http = new HTTP();
-    //     // let result = [];
-    //     http.get(environment.backendServicesURL, {}, {})
-    //     .then(data => {
-    //         //console.log(data.status);
-    //         //console.log(data.data); // data received by server
-    //         //console.log(data.headers);
-    //         //console.log(data.data);
-    //         console.log('REported spams:');
-    //         let items = JSON.parse(data.data)
-    //         console.log(items.objects);
-    //         console.log(typeof items);
-    //         return items;
-    //       })
-    //     .catch(error => {
-    //         console.log('error');
-    //         console.log(error);
-    //         console.log(error.status);
-    //         console.log(error.error); // error message as string
-    //         console.log(error.headers);
-    //         return error.error;
-    //       });
-    //       //return result;
-    // }
-
 }
