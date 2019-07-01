69fbe484d88f873bab13a7ff66f41c819b0a9d99
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-07-01 13:10:43 +0200

    chg: [news] RSS/ATOM feeds from sets are now parsed and displayed in the view.

--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -6,6 +6,9 @@ import {
     AlertController,
     ToastController } from 'ionic-angular';
 
+import * as Parser from 'rss-parser';
+
+import { fetch } from '../../lib/utils';
 import { UtilsService } from './utils';
 
 @Component({
@@ -15,6 +18,7 @@ import { UtilsService } from './utils';
 export class NewsPage {
     loading: Loading;
     feeds_sets: any[];
+    items: any[];
 
     constructor(
         public alertCtrl: AlertController,
@@ -23,18 +27,76 @@ export class NewsPage {
         public utils: UtilsService) {
 
         this.feeds_sets = [];
+        this.items = [];
 
         this.utils.loadNews()
         .then((result)=>{
-
             this.feeds_sets = result;
-            console.log(this.feeds_sets);
-
+            this.loadSlide(0);
         })
         .catch((err)=>{
             console.log("Error when retrieving list of news.");
         });
 
+    }
+
+
+    /*
+    * Refresh the feeds on slide change.
+    */
+    onSlideDidChange(slides) {
+        this.items = [];
+        let slidesIndex = 0;
+        try {
+            slidesIndex = slides.getActiveIndex();
+        } catch {
+            slidesIndex = 0;
+        }
+        // this.loadSlide(slidesIndex);
+        this.loadSlide(slidesIndex);
+    }
+
+    /*
+    * Load the slide at the specified index.
+    */
+    async loadSlide(index) {
+        this.items = [];
+        let tmpItems = [];
+        let parser = new Parser();
+
+
+
+        this.feeds_sets[index].feeds.map(function(feed) {
+
+            fetch(feed.link).then((response) => {
+                    console.log(feed.link);
+
+                    parser.parseString(response).then((val)=>{
+
+                        console.log(val.title);
+                        val.items.forEach(item => {
+                             console.log(item.title)
+                             tmpItems.push(item);
+                        });
+
+                    });
+
+
+
+            })
+            .catch((err)=>{
+                console.log("Error when parsing feed.");
+            });
+        });
+
+        this.items = tmpItems;
+
+
+
+    }
+
+    itemSelected(event) {
+        console.log(event);
     }
 
 
--- src/pages/news/utils.ts
+++ src/pages/news/utils.ts
@@ -31,4 +31,5 @@ export class UtilsService {
             });
         });
     }
+
 }
