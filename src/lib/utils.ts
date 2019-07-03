f65124168fa053a77c8500480e6951a4402c5876
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-07-03 14:33:06 +0200

    cleaned spaces

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -94,21 +94,21 @@ export function fetchURL(url): Promise<any> {
 * @return : the domain name
 */
 export function extractDomain(url) {
-  let domain = '';
-  // find & remove protocol (http, ftp, etc.) and get domain
-  if (url.indexOf("://") > -1) {
-    domain = url.split('/')[2];
-  } else {
-    domain = url.split('/')[0];
-  }
+    let domain = '';
+    // find & remove protocol (http, ftp, etc.) and get domain
+    if (url.indexOf("://") > -1) {
+        domain = url.split('/')[2];
+    } else {
+        domain = url.split('/')[0];
+    }
 
-  // find & remove www
-  if (domain.indexOf("www.") > -1) {
-    domain = domain.split('www.')[1];
-  }
+    // find & remove www
+    if (domain.indexOf("www.") > -1) {
+        domain = domain.split('www.')[1];
+    }
 
-  domain = domain.split(':')[0]; //find & remove port number
-  domain = domain.split('?')[0]; //find & remove url params
+    domain = domain.split(':')[0]; //find & remove port number
+    domain = domain.split('?')[0]; //find & remove url params
 
-  return domain;
+    return domain;
 }
