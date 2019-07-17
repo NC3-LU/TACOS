bcac18b02953f85b4ff3b66c75da426f651cfc54
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-07-17 14:01:43 +0200

    asks if the user want to sign the generated APK.

--- build.sh
+++ build.sh
@@ -8,5 +8,17 @@ npm install
 ionic cordova platforms add android
 ionic cordova build android --release
 
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $keystore ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $alias_name
-zipalign -v 4 ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk ./platforms/android/app/build/outputs/apk/release/TACOS.apk
+
+read -r -p "Do you want to sign the APK? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY])
+        jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $keystore ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $alias_name
+        zipalign -v 4 ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk ./platforms/android/app/build/outputs/apk/release/TACOS.apk
+        ;;
+    *)
+        :
+        ;;
+esac
+
+
+exit 0
