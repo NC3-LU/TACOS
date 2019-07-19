1d1e5979bd6508a0a19703df3fe7c78f8c6752b2
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-07-19 09:03:55 +0200

    fixed build script

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ ionic cordova build android --release
 read -r -p "Do you want to sign the APK? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY])
-        jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $key_alias ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $alias_name
+        jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $keystore ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $key_alias
         zipalign -v 4 ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $final_apk
         echo "Generation done. Signed APK available here:"
         echo $final_apk
