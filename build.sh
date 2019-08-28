ffced6a6159f8e4aa7ebd6c0b0082125f7dac58e
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-08-28 08:50:20 +0200

    only get the parameters if the user wants to sign the APK

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #! /usr/bin/env bash
 
-keystore=$1
-key_alias=$2
-
 final_apk=./platforms/android/app/build/outputs/apk/release/TACOS.apk
-
 
 rm -Rf node_modules/ www/ plugins/ platforms/android/
 npm install
@@ -15,6 +11,8 @@ ionic cordova build android --release
 read -r -p "Do you want to sign the APK? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY])
+        keystore=$1
+        key_alias=$2
         jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $keystore ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $key_alias
         zipalign -v 4 ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $final_apk
         echo "Generation done. Signed APK available here:"
