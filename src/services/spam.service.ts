3b14696bdd39fa947c0afb50835fbdc06d2b91c2
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-09-05 12:51:15 +0200

    new: [service] added new service to store configurations related to the spam module

---
+++ src/services/spam.service.ts
@@ -0,0 +1,35 @@
+import { Platform } from 'ionic-angular';
+import { Injectable } from '@angular/core';
+
+import { Storage } from '@ionic/storage';
+
+const SPAM_SEND_CLEAR = 'SPAM_SEND_CLEAR';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpamService {
+    constructor(
+        public platform: Platform,
+        private storage: Storage) {
+    }
+
+    setInitialSpamSettings() {
+        this.storage.get(SPAM_SEND_CLEAR).then(val => {
+            if (val == null) {
+                this.storage.set(SPAM_SEND_CLEAR, true);
+            }
+        });
+    }
+
+    /*
+    * Toggle the value of clear
+    */
+    setSpamSendClear() {
+        this.storage.get(SPAM_SEND_CLEAR).then(val => {
+            // toggle the value
+            this.storage.set(SPAM_SEND_CLEAR, !val);
+        });
+    }
+}
